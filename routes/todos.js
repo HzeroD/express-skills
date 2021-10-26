1959366318834652bf4bf38e2d98e52b837fa2f5
ManliestBen <benjamintmanley@gmail.com>
2021-10-26 10:02:15 -0500

    refactor controller code

--- routes/todos.js
+++ routes/todos.js
@@ -1,17 +1,10 @@
 import { Router } from 'express'
-import * as todoDb from '../data/todo-db.js'
+import * as todosCtrl from '../controllers/todos.js'
 const router = Router()
 
 /* GET users listing. */
 
-router.get('/', function(req, res) {
-  todoDb.find({}, function(error, todos) {
-    res.render('todos/index', {
-      todos: todos,
-      error: error
-    })
-  })
-})
+router.get('/', todosCtrl.index)
 
 export {
   router
